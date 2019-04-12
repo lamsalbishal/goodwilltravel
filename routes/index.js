e8c6fbea2ecec1a1240a300d49ed294152c3949d
Bishal Lamsal <bishalrajlamsal65@gmail.com>
2019-04-12 10:37:22 +0545

    connect to database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var homeModel = require('../modles/home.modle');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,6 +9,18 @@ router.get('/', function(req, res, next) {
 
 router.get('/product',function(req,res,next){
   res.render('product')
-})
+});
+
+router.get('/allhome',function(req,res,next)
+	{
+   
+		homeModel.find().exec(function(err,product){
+			if(err)
+			{
+				return next(err);
+			}
+			res.json(product);
+		})
+	});
 
 module.exports = router;
